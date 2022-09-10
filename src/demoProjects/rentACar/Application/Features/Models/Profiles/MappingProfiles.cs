1f5a8d25d8961fa6425f45710216899bff543f2b
Muhammed Görkem Acır <86821706+gorkemacir@users.noreply.github.com>
2022-09-10 21:58:02 +0300

    Day 4 kompleks modelleme ve Dynamic Query

---
+++ src/demoProjects/rentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Models.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Model, ModelListDto>()
+                .ForMember(c=>c.BrandName, opt=> opt.MapFrom(c=>c.Brand.Name))
+                .ReverseMap();
+        }
+    }
+}
--- src/demoProjects/rentACar/Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -10,6 +10,7 @@ namespace Domain.Entities
     public class Brand:Entity
     {
         public string Name { get; set; }
+        public virtual ICollection<Model> Models{ get; set; }
 
         public Brand()
         {
