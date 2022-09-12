144d54b96017ecb4564833b77f8a8c1e68f26eb2
Muhammed Görkem Acır <86821706+gorkemacir@users.noreply.github.com>
2022-09-12 22:02:03 +0300

    homework done

---
+++ src/demoProjects/kodlamaIoDevs/Domain/Entities/ProgramingLanguage.cs
@@ -0,0 +1,24 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProgramingLanguage:Entity
+    {
+        public string Name { get; set; }
+        public ProgramingLanguage()
+        {
+        }
+
+        public ProgramingLanguage(int id, string name):this()
+        {
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
